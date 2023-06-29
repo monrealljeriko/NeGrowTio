616b773bfa9a548df7a04e8e0f5e7edb92330cc6
monreall.jeriko <monreall.jeriko@gmail.com>
2023-06-29 17:45:26 +0800

    edit start btn

--- src/app/start/start.page.ts
+++ src/app/start/start.page.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './start.page.html',
-  styleUrls: ['./start.page.scss'],
+    selector: 'app-start',
+    templateUrl: './start.page.html',
+    styleUrls: ['./start.page.scss'],
 })
 export class StartPage implements OnInit {
 
-  constructor(private router:Router) { }
+    constructor(private router:Router) { }
 
-  ngOnInit() {
-  }
-
-    handleButtonWelcome() {
-        this.router.navigate(['./welcome']);
+    ngOnInit() {
     }
+
+        handleButtonWelcome() {
+            this.router.navigate(['./welcome']);
+        }
   
 }

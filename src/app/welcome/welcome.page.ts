5547e4413d5a834d4ebaec07a3ff12080d61dfe1
monreall.jeriko <monreall.jeriko@gmail.com>
2023-06-29 17:48:49 +0800

    add logo & add logo

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.page.html',
-  styleUrls: ['./welcome.page.scss'],
+    selector: 'app-welcome',
+    templateUrl: './welcome.page.html',
+    styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+    constructor(private router:Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    handleButtonLogin() {
+        this.router.navigate(['./login']);
+    }
 
 }

6240b7359803b379a6ec872e0e7bc9c8cd7e5db8
monrealljeriko <monreall.jeriko@gmail.com>
2023-07-09 00:17:55 +0800

    add notification & design improvements

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,11 @@ export class HomePage {
 
   constructor() {}
    showLabel: boolean = false;
+   isModalOpen = false;
 
+   setOpen(isOpen: boolean) {
+     this.isModalOpen = isOpen;
+   }
   toggleLabel() {
     this.showLabel = !this.showLabel;
   }
